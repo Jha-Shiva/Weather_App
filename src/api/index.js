6c4eccf953f9f71dfac64c5fe47f1c339090998f
Jha-Shiva <shivamjha625@gmail.com>
2025-08-03 19:29:09 +0530

    changes in api folder

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
-const baseURL = import.meta.env.VITE_BASE_URL;
+// const baseURL = import.meta.env.VITE_BASE_URL;
+const baseURL = 'https://api.weatherapi.com/v1/forecast.json?key=73c0476cb7a848ce914115522252307'
 
 export const getWeatherDataForCity = async (city) => {
   const response = await fetch(`${baseURL}&q=${city}&days=7&aqi=yes`);
